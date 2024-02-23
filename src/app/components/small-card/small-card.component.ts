40b7c86284e04ee48c64627cb466a74affc267e6
CArlos Arruda <edu.arruda.759@gmail.com>
2024-02-23 16:55:54 -0300

    novas alterações

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+
+  @Input()
+  photoTitle:string = ""
+
+  @Input()
+  cardDescription:string = "A linguagem java é uma linguagem que nasceu no ano de 1995 por James Gosling na empresa Sun Microsystemns, atualmente é uma das linguagens mais utilizadas no mercado contanto com suporte para back-end, front-end, mobile e nos ultimos anos até mesmo com cloud computing com o framework Quarkus."
+
+  constructor(){
+
+  }
+  
+  ngOnInit(): void {
+    
+  }
 
 }
